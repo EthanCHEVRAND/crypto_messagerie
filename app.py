d2d3ba418af25c55b405c8ec8a6018a5865e206a
EthanCHEVRAND <ethan.chevrand@edu.univ-fcomte.fr>
2025-11-19 11:25:46 +0100

    finished project

--- app.py
+++ app.py
@@ -1,10 +1,72 @@
+import os
 from tools.crypto import *
 from tools.files import *
 
 def main():
-    f = create_file("test.txt", "salut c'est moi tchoupi")
-    print(f.read())
+    print("Bienvenue sur notre messagerie\n \
+    1) Creer un couple de cles\n \
+    2) Crypter un message\n \
+    3) Decrypter un message\n \
+    4) Quitter")
+    choice = input("Rentrez une option >>> ")
+    if (int(choice) == 1):
+        key_creation()
+    if (int(choice) == 2):
+        message_crypting()
+    if (int(choice) == 3):
+        message_decrypting()
+    
     return 0
+
+def key_creation():
+    name = input("Entrez votre nom : ")
+    priv, pub = create_keys(name)
+    return 0
+
+def message_crypting():
+    message = input("Rentrez votre message >>> ")
+    dest = input("Qui est le destinataire ? >>> ")
+
+    folder = "my_keys"
+    with open(f"{folder}/cle_publique_{dest}.pub", "r") as f:
+        lignes = f.read().splitlines()
+
+    n = int(lignes[0])
+    e = int(lignes[1])
+
+    print(n, e)
+
+    C = ""
+    for i in range(len(message)):
+        C += str(codageRSA(ord(message[i]), n, e)) + "\n"
+    print(f"{C}")
+
+    f = create_file("message.txt", C)
+
+    return C
+
+def message_decrypting():
+    filename = input("Quel est le fichier à décrypter ? >>> ")
+    folder = "my_keys"
+
+    with open(f"{folder}/cle_privee", "r") as f:
+        lignes = f.read().splitlines()
+
+    n = int(lignes[0])
+    d = int(lignes[1])
+
+    with open(f"{filename}", "r") as f:
+        lignes = f.read().splitlines()
+    M = ""
+    for ligne in lignes:
+        M += chr(decodageRSA(int(ligne), n, d))
+
+    print(M)
+
+    return 0
+
+    
+
 
 if __name__ == "__main__":
     main()
--- tools/files.py
+++ tools/files.py
@@ -1,3 +1,6 @@
+import os
+from .crypto import *
+
 def create_file(filename, content):
     f = open(filename, "x")
     
@@ -6,4 +9,29 @@ def create_file(filename, content):
 
     f = open(filename)
     return f
+
+def create_keys(name):
+    folder = "my_keys"
+    os.makedirs(folder, exist_ok=True)
+
+    p, q, e = choixCle(100, 500)
+    n_priv, d_priv = clePublique(p, q, e)
+    n_pub, e_pub = clePrivee(p, q, e)
+
+    pub = f"{n_pub}\n{e_pub}"
+    priv = f"{n_priv}\n{d_priv}"
+    
+    priv_path = os.path.join(folder, f"cle_privee_{name}")
+    pub_path  = os.path.join(folder, f"cle_publique_{name}.pub")
+
+    with open(priv_path, "w") as f:
+        f.write(priv)
+
+    with open(pub_path, "w") as f:
+        f.write(pub)
+
+    priv_key = open(priv_path)
+    pub_key = open(pub_path)
+
+    return priv_key, pub_key
 
